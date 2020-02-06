0d391ab9f3ec1e22029896cd6689867eaea9d7c1
Tomasz Zema <tomasz.zema@gmail.com>
2020-02-06 21:39:11 +0100

    it works

--- crawly no 4.py
+++ crawly no 4.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url = 'http://www.olx.pl/oferty/q-asus-rog/'
 response = get(url)
-print(response.text[:500])
+# print(response.text[:500])
 html_soup = BeautifulSoup(response.text, 'html.parser')
 type(html_soup)
 
@@ -22,12 +22,29 @@ print(len(linki_l))
 new_list = [x for x in linki_l if len(x) > 76]
 print(new_list)
 print(len(new_list))
+#  operacja na jednym
 
+# site = new_list[0]
+# print(type(site))
+# print("strona internetowa:", site)
+# response_sub_site = get(site)
+# print ("test................")
+# html_soup_sub = BeautifulSoup(response_sub_site, 'html.parser')
+# # for site in new_list:
+# print ("test................")
+url = new_list[0]
+url2 = 'https://www.olx.pl/oferta/asus-rog-strix-gl502vs-16gb-512ssd-gtx1070-faktura-vat-23-CID99-IDCwx4b.html#a0e78786f1'
+response = get(url)
+print(response.text[:500])
+html_soup = BeautifulSoup(response.text, 'html.parser')
+type(html_soup)
 
-# item_containers = html_soup.find_all('tr', class_='wrap')
-# item_containers_offer = html_soup.find_all('div', class_='offer-wrapper')
-# print(type(item_containers))
-# print(len(item_containers))
+item_containers = html_soup.find_all('tr', class_='wrap')
+item_containers_offer = html_soup.find_all('div', class_='offer-wrapper')
+item_containers_desc = html_soup.find_all('div', class_='clr lheight20 large')
+print ("test................")
+# print(html_soup)
+print(type(item_containers_desc))
 # print(item_containers[0])
 #
 # print("test2")
